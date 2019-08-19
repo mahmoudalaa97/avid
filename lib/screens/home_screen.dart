59bd5a7ecf6480464e4b49545e54433963f2f3b4
mahmoud alaa ali <mahmoudadmob@gmail.com>
2019-08-19 06:33:26 +0200

    1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,39 +1,43 @@
-
 import 'package:avid/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  ///------------------------------------------------  Parameters Section -------------------------------------///
+  // black Color For BackGround
+  final int colorBackground = 0xff333333;
   final BaseAuth auth;
   final VoidCallback onSignedOut;
-  const HomeScreen({this.auth,this.onSignedOut}) ;
 
-  void _signOut()async{
-   try{
-     await auth.signOut();
-     onSignedOut();
-   }catch(e){
+  const HomeScreen({this.auth, this.onSignedOut});
 
-   }
-  }
-
+  ///------------------------------------------------  BuildWidget Section -------------------------------------///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("User Info"),
+        backgroundColor: Color(colorBackground),
+        title: Text("Avid."),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(""),
+            Text("Welcome to home screen"),
             RaisedButton(
               child: Text("SIGN OUT"),
-              onPressed:_signOut,
+              onPressed: _signOut,
             )
           ],
         ),
       ),
     );
+  }
+
+  ///------------------------------------------------  Method  Section -------------------------------------///
+  void _signOut() async {
+    try {
+      await auth.signOut();
+      onSignedOut();
+    } catch (e) {}
   }
 }
